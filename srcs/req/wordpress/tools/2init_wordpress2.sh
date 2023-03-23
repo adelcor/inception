968598d2cf3be10aee7e31d3443849fde1d844f9
adelcor <adelcorralm@gmail.com>
2023-03-23 12:21:59 +0100

    all

--- srcs/req/wordpress/tools/2init_wordpress2.sh
+++ srcs/req/wordpress/tools/2init_wordpress2.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-#mkdir -p /var/www/html/
-#echo "Me estoy ejecutando estoy harto ya"
-#while ! nc -z mariadb 3306; do
-#  sleep 1
-#done
+
+mkdir -p /var/www/html/
+
 cd /var/www/html
+
 wp core download --locale=es_ES --allow-root
 wp --path=/var/www/html config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --locale=es_ES --allow-root --skip-check
 wp core install --path=/var/www/html  --url=${DOMAIN_NAME} --title="Hello 42Urduliz" --admin_name=${WP_ADMIN_USER} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email="adel-cor@student.42urduliz.com" --skip-email --allow-root
